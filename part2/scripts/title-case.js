846339818b068a784a02ea73980b2e05a7da9612
The-Sw33t3r-Man <sylvesterezeh@trentu.ca>
2024-10-24 03:13:40 -0400

    final stage

--- part2/scripts/title-case.js
+++
@@ -1,9 +0,0 @@
-// titleCase.js
-function titleCase(str) {
-    return str.toLowerCase().split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
-}
-
-// Test the function with a sample string
-const sampleText = "hello world from javascript";
-console.log(`Original: ${sampleText}`);
-console.log(`Title Cased: ${titleCase(sampleText)}`);
